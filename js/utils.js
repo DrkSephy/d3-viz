7f91e00972df73fff95237436349dbe7cd051afb
DrkSephy <dleonar00@citymail.cuny.edu>
2016-03-26 18:39:31 -0400

    Fixing tabbing

--- js/utils.js
+++ js/utils.js
@@ -4,13 +4,13 @@ module.exports = {
   fetchJSON: function(url) {
     return new Promise((resolve, reject) => {
       request.get(url, { 'headers': {'User-Agent': 'DrkSephy' }},
-        (error, response, body) => {
-          if (body) {
-              resolve(JSON.parse(body));
-            } else {
-              resolve({});
-            }
-        });
+      (error, response, body) => {
+        if (body) {
+          resolve(JSON.parse(body));
+        } else {
+          resolve({});
+        }
+      });
     });
   }
 }
