8bf0367c815f8f07359f2e00f31f988f0930c128
DrkSephy <dleonar00@citymail.cuny.edu>
2016-04-26 21:31:17 -0400

    Cleaning up main.js

--- timeSeries/js/main.js
+++ timeSeries/js/main.js
@@ -3,11 +3,14 @@ var margin = {top: 30, right: 20, bottom: 30, left: 50},
     width = 600 - margin.left - margin.right,
     height = 270 - margin.top - margin.bottom;
 
+// Color hash will be used for coloring each line
 var color_hash = {  
   0 : ["#ff6961"],
   1 : ['#61a8ff']
 } 
 
+// Used for creating the legend
+// Colors correspond to the line color order
 var legend_hash = {
   0 : ['Commits', "#ff6961"],
   1 : ['Stars', '#61a8ff']
@@ -27,32 +30,38 @@ var xAxis = d3.svg.axis().scale(x)
 var yAxis = d3.svg.axis().scale(y)
     .orient("left").ticks(5);
 
-// Define the line
+// Define the line object
 var line = d3.svg.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.count); })
     
-// Adds the svg canvas
+// Add the svg canvas
 var svg = d3.select("body")
-    .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// Load our dataset
 d3.json('weekly.json', function(err, data) {
+
+  // Parse the dates and counts
   data.forEach(function(d) {
       d.date = parseDate(d.date);
       d.count = d.count;
   });
 
+  // Scale dates and count values
   x.domain(d3.extent(data, function(d) { return d.date; }));
   y.domain([0, d3.max(data, function(d) { return d.count; })]); 
 
+  // Used to group objects by keys
   var dataNest = d3.nest()
       .key(function(d) {return d.name;})
       .entries(data);
 
+  // For each array of keys (commits, stars, etc), create a line
   dataNest.forEach(function(d) {
     svg.append("path")
       .attr("class", "line")
@@ -60,6 +69,7 @@ d3.json('weekly.json', function(err, data) {
       .attr("d", line(d.values));
   });
 
+  // Initialize Tooltip object
   var tip = d3.tip()
     .attr('class', 'd3-tip')
     .offset([-10, 0])
@@ -67,8 +77,10 @@ d3.json('weekly.json', function(err, data) {
       return '<strong>' + d.name + ':' +  "</strong> <span style='color:white'>" +  d.count + '</span>'
     });
 
+  // Display tooltips
   svg.call(tip);
 
+  // Create circles around all points
   var circles = svg.selectAll("circle")
     .data(data)
     .enter()
@@ -84,16 +96,18 @@ d3.json('weekly.json', function(err, data) {
     .on('mouseout', tip.hide)
 
 
+  // Create x-axis
   svg.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + height + ")")
     .call(xAxis);
 
+  // Create y-axis
   svg.append("g")
     .attr("class", "y axis")
     .call(yAxis);
 
-  // add legend   
+  // Initialize legend  
   var legend = svg.append("g")
     .attr("class", "legend")
     .attr("x", 500 - 65)
@@ -101,7 +115,9 @@ d3.json('weekly.json', function(err, data) {
     .attr("height", 100)
     .attr("width", 100);
 
-  legend.selectAll('g').data(dataNest)
+  // Create the legend
+  legend.selectAll('g')
+    .data(dataNest)
     .enter()
     .append('g')
     .each(function(d, i) {
@@ -120,6 +136,5 @@ d3.json('weekly.json', function(err, data) {
         .attr("width",100)
         .style("fill", color_hash[String(i)])
         .text(legend_hash[String(i)][0]);
-
     });
 });
